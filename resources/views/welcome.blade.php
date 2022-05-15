af3906fcf6e67be1480cd715859fa13b1e6c421a
EvanHendersonRichtByte <adamhenderson3x3@gmail.com>
2022-05-15 23:38:06 +0700

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <title>FlexLib</title>
+
+</head>
+
+<body>
+    <nav class="flex justify-center p-4 bg drop-shadow-xl">
+        <a class="ml-4" href="/dashboard">Dashboard</a>
+        <a class="ml-4" href="/books">Books</a>
+        <a class="ml-4" href="/profile">Profile</a>
+    </nav>
+</body>
+
+</html>
